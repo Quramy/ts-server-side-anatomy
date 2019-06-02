808bda8884bb6d9f8ab7604833c6c6fb185fd0c7
Yosuke Kurami <yosuke.kurami@gmail.com>
2019-06-03 03:59:51 +0900

    add parse performance script

---
+++ ls-sample/src/parse-performance.ts
@@ -0,0 +1,44 @@
+import * as ts from "typescript";
+import path from "path";
+import fs from "fs";
+
+const insertText = "\ndeclare namespace hoge { export function fuga(): void; }"
+
+const rangeItems = (n: number) => {
+  const arr = [];
+  for(let i = 0; i < n; i++) {
+    arr[i] = i;
+  }
+  return arr;
+};
+
+function main() {
+  const originalSourceText = `declare namespace fuga {
+${rangeItems(10000).map((_, i) => "  function fn" + i + "(): number;").join("\n")}
+}
+  `;
+
+  const origSource = ts.createSourceFile("index.d.ts", originalSourceText, ts.ScriptTarget.ESNext, true, ts.ScriptKind.TS);
+
+  const s1 = Date.now();
+  let sourceFile = origSource;
+  for (let i = 0; i < insertText.length; i++) {
+    const pos = sourceFile.end;
+    sourceFile = ts.updateSourceFile(sourceFile, sourceFile.getText() + insertText[i], { newLength: 1, span: { start: pos, length: 0 } });
+  }
+  const e1 = Date.now();
+
+  const s2 = Date.now();
+  for (let i = 0; i < insertText.length; i++) {
+    const origSource = ts.createSourceFile("index.d.ts", originalSourceText + insertText.slice(0, i), ts.ScriptTarget.ESNext, true, ts.ScriptKind.TS);
+  }
+  const e2 = Date.now();
+
+  console.log(`originalSourceText lines: ${originalSourceText.split("\n").length}`);
+  console.log(`originalSourceText length: ${originalSourceText.length}`);
+  console.log(`insertText length: ${insertText.length}`);
+  console.log(`elapsed time with incremental parsing: ${e1 - s1}`);
+  console.log(`elapsed time with non-incremental parsing: ${e2 - s2}`);
+}
+
+main();
